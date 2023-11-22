d487b7ac97c267aebe28f29a9f43b36f0584c581
Ehimwenman Edemakhiota <edemaehiz@gmail.com>
2023-11-22 18:24:24 +0100

    minor edit

--- services/service1/src/main/java/org/example/service1/Service1.java
+++ services/service1/src/main/java/org/example/service1/Service1.java
@@ -6,11 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Service1 {
 
-
-
-
-
-
     public static void main(String[] args) {
         SpringApplication.run(Service1.class, args);
     }
